2ecc8c9d01d61ca113ffd54cbabe59c044fa3269
drvanon <robbiedorstijn@gmail.com>
2013-09-16 08:43:49 +0200

    Client side add dynamic blog making

--- blog/static/admin.js
+++ blog/static/admin.js
@@ -1,8 +1,26 @@
 $(document).ready(function () {
+  var alineas = 0;
+  var images = 0;
+
   $('#new_blog').submit(function (e) {
     e.preventDefault();
     $.post('/blog/admin/new_blog', $(this).serialize(), function (data){
       alert(data.message);
     });
+  });
+
+  $("#new_element").submit(function (e) {
+    e.preventDefault();
+    if($("#select_type").val()=="alinea"){
+      alineas++;
+      var lab = document.createElement("label");
+      var input = document.createElement("input");
+
+      input.type = "textarea";
+      input.name = "alinea" + alineas;
+      lab.innerHTML = "Alinea:";
+      lab.appendChild(input);
+      $('#inputField').append(lab);
+    }
   });
 });
