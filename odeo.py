ed03ff0744e952b51852d194e1e46f01b986bac6
drvanon <robbiedorstijn@gmail.com>
2013-09-22 18:20:46 +0200

    Add nginx

--- odeo.py
+++ odeo.py
@@ -143,4 +143,4 @@ def delete_blog(id):
 app.secret_key = '''X\r|R\xe7y\x1bl\xd1\xb2\xf8)
     \xbe\xb7\x1a\x90\x03\xf8=\\\x14SF`'''
 
-app.run(host="localhost", debug=True)
+app.run(host="localhost")
